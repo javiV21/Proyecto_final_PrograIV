9d9ba3af979a90a13befb13a12edd8091546f427
WGuandique-2005 <wguandiquerivera07@gmail.com>
2025-06-06 12:44:52 -0600

    commit

--- pf/app/Http/Controllers/NewsletterController.php
+++ pf/app/Http/Controllers/NewsletterController.php
@@ -12,15 +12,14 @@ class NewsletterController extends Controller
 {
     public function subscribe(Request $request)
     {
-        $data = $request->validate([
-            'email' => 'required|email',
-        ]);
-
         try {
+            $data = $request->validate([
+                'email' => 'required|email',
+            ]);
+            
             Mail::to($data['email'])->send(new NewsletterSubscribed());
             Log::info('Correo de confirmación enviado a: ' . $data['email']);
             return back()->with('success', '¡Correo de confirmación enviado!');
-
         } catch (Exception $e) {
             Log::error('Error al enviar correo a: ' . $data['email'] . '. Error: ' . $e->getMessage());
             return back()->with('error', '¡Error al enviar el correo de confirmación!');
